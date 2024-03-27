e00f6d4c69ca6a574e114a40ad332bec360bcb6c
Nicola Peduzzi <thenikso@gmail.com>
2024-03-27 10:14:23 +0100

    Add pipe and lset

--- test/basic.test.js
+++ test/basic.test.js
@@ -8,7 +8,7 @@ describe('basic', (assert) => {
 
   assertLogs('print "Hello, World!"', ['Hello, World!']);
 
-  assertLogs('print 10 .inc', [11]);
+  assertLogs('print 10 .inc', ['11']).andReturn(11);
 
   assertLogs(
     `{ 7 4 12 21 } .with {
@@ -19,7 +19,11 @@ describe('basic', (assert) => {
       .sum .printv "Sum: {}"
     }`,
     ['Max: 21', 'Min: 4', 'Avg: 11', '---------', 'Sum: 44'],
-  );
+  ).andReturn(44);
 
-  assertLogs(`x: "hello" y: "word" print x`, ['hello']);
+  assertLogs(`x: "hello" y: "word" print x y`, ['hello']).andReturn('word');
+
+  assertLogs(`inc 4 :x print x`, ['5']).andReturn(5);
+
+  assertLogs(`inc 14 .print |print`, ['14', '15']);
 });
--- test/runner/utils.mjs
+++ test/runner/utils.mjs
@@ -19,92 +19,116 @@ export function testUtils(context, assert, defaultOptions) {
   const createContext = () => {
     const ctx = Object.create(context, {
       print: {
-        value: (s) => currentLogs.push(s),
+        value: (s) => {
+          currentLogs.push(s?.toString?.() ?? s);
+          return s;
+        },
         writable: false,
       },
       printv: {
-        value: (v, s) => currentLogs.push(s.replace('{}', v)),
+        value: (v, s) => {
+          currentLogs.push(s.replace('{}', v));
+          return v;
+        },
         writable: false,
       },
     });
     return ctx;
   };
 
+  function assertReturn(code, ret, options) {
+    const { showTime = defaultShowTime, given } = options ?? {};
+    const startTime = showTime ? performance.now() : 0;
+    assert({
+      given: given ?? `\`${code}\``,
+      should: `return ${typeof ret === 'string' ? `"${ret}"` : ret}`,
+      actual: (currentContext || createContext()).eval(code),
+      expected: ret,
+    });
+    if (showTime) {
+      const endTime = performance.now();
+      console.log(`took ${endTime - startTime}ms`);
+    }
+    return {
+      andLog: (logs) =>
+        assertLogs(code, logs, { ...options, given: 'the above' }),
+    };
+  }
+
+  function assertError(code, error, options) {
+    const { showTime = defaultShowTime } = options ?? {};
+    const startTime = showTime ? performance.now() : 0;
+    assert({
+      given: `\`${code}\``,
+      should: `throw "${error}"`,
+      actual: (() => {
+        let res;
+        let err;
+        try {
+          res = (currentContext || createContext()).eval(code);
+        } catch (e) {
+          err = e;
+        }
+        return err?.message ?? res;
+      })(),
+      expected: error,
+    });
+    if (showTime) {
+      const endTime = performance.now();
+      console.log(`took ${endTime - startTime}ms`);
+    }
+  }
+
+  function assertCode(code, expected, options) {
+    const { showTime = defaultShowTime } = options ?? {};
+    const startTime = showTime ? performance.now() : 0;
+    assert({
+      given: `\`${code}\``,
+      should: `parse as \`${expected}\``,
+      actual: (currentContext || createContext()).parse(code).toString(),
+      expected,
+    });
+    if (showTime) {
+      const endTime = performance.now();
+      console.log(`took ${endTime - startTime}ms`);
+    }
+  }
+
+  function assertLogs(code, logs, options) {
+    const { showTime = defaultShowTime, mapLogs, given } = options ?? {};
+    logs = Array.isArray(logs) ? logs : [logs];
+    const startTime = showTime ? performance.now() : 0;
+    assert({
+      given: given ?? `\`${code}\``,
+      should: `log ${logs.map((l) => `"${l}"`).join(', ')}`,
+      actual: (() => {
+        currentLogs = [];
+        (currentContext ?? createContext()).eval(code);
+        return mapLogs ? mapLogs(currentLogs) : currentLogs;
+      })(),
+      expected: logs,
+    });
+    if (showTime) {
+      const endTime = performance.now();
+      console.log(`took ${endTime - startTime}ms`);
+    }
+    return {
+      andReturn: (ret) =>
+        assertReturn(code, ret, { ...options, given: 'the above' }),
+    };
+  }
+
+  function withContext(fn, ctx) {
+    currentContext = ctx ?? createContext();
+    fn(currentContext);
+    currentContext = null;
+  }
+
   return {
-    assertReturn(code, ret, options) {
-      const { showTime = defaultShowTime } = options ?? {};
-      const startTime = showTime ? performance.now() : 0;
-      assert({
-        given: `\`${code}\``,
-        should: `return ${typeof ret === 'string' ? `"${ret}"` : ret}`,
-        actual: (currentContext || createContext()).eval(code),
-        expected: ret,
-      });
-      if (showTime) {
-        const endTime = performance.now();
-        console.log(`took ${endTime - startTime}ms`);
-      }
-    },
-    assertError(code, error, options) {
-      const { showTime = defaultShowTime } = options ?? {};
-      const startTime = showTime ? performance.now() : 0;
-      assert({
-        given: `\`${code}\``,
-        should: `throw "${error}"`,
-        actual: (() => {
-          let res;
-          let err;
-          try {
-            res = (currentContext || createContext()).eval(code);
-          } catch (e) {
-            err = e;
-          }
-          return err?.message ?? res;
-        })(),
-        expected: error,
-      });
-      if (showTime) {
-        const endTime = performance.now();
-        console.log(`took ${endTime - startTime}ms`);
-      }
-    },
-    assertCode(code, expected, options) {
-      const { showTime = defaultShowTime } = options ?? {};
-      const startTime = showTime ? performance.now() : 0;
-      assert({
-        given: `\`${code}\``,
-        should: `parse as \`${expected}\``,
-        actual: (currentContext || createContext()).parse(code).toString(),
-        expected,
-      });
-      if (showTime) {
-        const endTime = performance.now();
-        console.log(`took ${endTime - startTime}ms`);
-      }
-    },
-    assertLogs(code, logs, options) {
-      const { showTime = defaultShowTime, mapLogs } = options ?? {};
-      logs = Array.isArray(logs) ? logs : [logs];
-      const startTime = showTime ? performance.now() : 0;
-      assert({
-        given: `\`${code}\``,
-        should: `log ${logs.map((l) => `"${l}"`).join(', ')}`,
-        actual: (() => {
-          currentLogs = [];
-          (currentContext ?? createContext()).eval(code);
-          return mapLogs ? mapLogs(currentLogs) : currentLogs;
-        })(),
-        expected: logs,
-      });
-      if (showTime) {
-        const endTime = performance.now();
-        console.log(`took ${endTime - startTime}ms`);
-      }
-    },
-    withContext(fn, ctx) {
-      currentContext = ctx ?? createContext();
-      fn(currentContext);
-      currentContext = null;
-    },
+    assertReturn,
+    assertError,
+    assertCode,
+    assertLogs,
+    withContext,
   };
 }
