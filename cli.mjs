bafea6f693f83d18f98b05a4db0d47481e6e634e
Nicola Peduzzi <thenikso@gmail.com>
2024-03-29 17:57:55 +0100

    Add CLI completer

--- cli.mjs
+++ cli.mjs
@@ -26,13 +26,27 @@ function repl() {
     prompt: chalk.greenBright('⟩ '),
     useColors: true,
     useGlobal: false,
-    eval: async function (cmd, replContext, filename, callback) {
+    preview: true,
+    eval: function (cmd, replContext, filename, callback) {
       try {
         const res = advance.exec(cmd, replContext._advContext);
         callback(null, res);
       } catch (err) {
         callback(err);
       }
+    },
+    completer: function (line) {
+      const completions = [];
+      let ctx = replServer.context._advContext;
+      while (ctx) {
+        completions.push(
+          ...Object.getOwnPropertySymbols(ctx).map(Symbol.keyFor),
+        );
+        ctx = Object.getPrototypeOf(ctx);
+      }
+      const word = line.substring(line.lastIndexOf(' ') + 1);
+      const hits = completions.filter((c) => c.startsWith(word));
+      return [hits.length ? hits : completions, word];
     },
   });
 
